bb8a33da9c112815578a40cbc77f4cf8fd049441
Pavel Cerny <cerny.pav@gmail.com>
2016-08-21 13:20:02 +0200

    client talks with Watson

--- src/main/java/cz/cvut/fel/controller/PoiDialogController.java
+++ src/main/java/cz/cvut/fel/controller/PoiDialogController.java
@@ -1,5 +1,8 @@
 package cz.cvut.fel.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -8,10 +11,17 @@ import com.ibm.watson.developer_cloud.service.exception.InternalServerErrorExcep
 import com.ibm.watson.developer_cloud.service.exception.UnauthorizedException;
 import cz.cvut.fel.model.DtoFromClient;
 import cz.cvut.fel.model.DtoTextAndContext;
+import org.codehaus.groovy.runtime.metaclass.ConcurrentReaderHashMap;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/poi")
@@ -23,7 +33,7 @@ public class PoiDialogController {
     private static final String CONVERSATION_VERSION = "2016-07-11";
 
 
-    @RequestMapping("/init")
+    @RequestMapping("/rest/init")
     @ResponseBody
     public MessageResponse initDialog() {
 
@@ -42,24 +52,12 @@ public class PoiDialogController {
                 .message(workspaceId, newMessage)
                 .execute();
 
-        System.out.println(response);
-
-        try {
-
-        } catch (IllegalArgumentException e) {
-            // Missing or invalid parameter
-        } catch (BadRequestException e) {
-            // Missing or invalid parameter
-        } catch (UnauthorizedException e) {
-            // Access is denied due to invalid credentials
-        } catch (InternalServerErrorException e) {
-            // Internal Server Error
-        }
+        //System.out.println(response);
 
         return response;
     }
 
-    @RequestMapping(value = "/continue", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/rest/continue", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE}, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public MessageResponse continueDialog(@RequestBody DtoFromClient dtoFromClient) {
 
@@ -81,18 +79,74 @@ public class PoiDialogController {
     }
 
     @RequestMapping
-    public String poi(@RequestParam(value = "inFromWatson", required=false, defaultValue = "Ahoj, kam chces?") String inFromWatson,
-                      @RequestParam(value = "inContext", required=false, defaultValue = "{'vychozizprava':'obsah'}") String inContext,
-                      Model model){
+    public String initPoi(Model model){
+        //init new conversation with Watson
+        MessageResponse response = initDialog();
+
+        //get Watson's reply
+        String inFromWatson = getFromWatsonUnified(response);
+
+        //get context details
+        String inContext = getContextInJson(response);
+
+        //send it to view
         model.addAttribute("inFromWatson", inFromWatson)
                 .addAttribute("inContext", inContext)
                 .addAttribute("dtoTextAndContext", new DtoTextAndContext());
         return "poi";
     }
 
-    @RequestMapping(value="/result", method=RequestMethod.POST)
-    public String greetingSubmit(@ModelAttribute DtoTextAndContext dtoTextAndContext, Model model) {
-        model.addAttribute("dtoTextAndContext", dtoTextAndContext);
-        return "result";
+    @RequestMapping(value="/continue", method=RequestMethod.POST)
+    public String continuePoi(@ModelAttribute DtoTextAndContext dtoTextAndContext, Model model) {
+        //parse input from JSON to objects
+        Map<String, Object> input = new HashMap<>();
+        input.put("text",dtoTextAndContext.getInput());
+
+        HashMap<String,Object> context;
+        try {
+            context =
+                    new ObjectMapper().readValue(dtoTextAndContext.getContext(), HashMap.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            context = new HashMap<>();
+        }
+
+        //continue dialog with Watson
+        DtoFromClient dtoFromClient = new DtoFromClient(input, context);
+        MessageResponse response = continueDialog(dtoFromClient);
+
+        //get Watson's reply
+        String inFromWatson = getFromWatsonUnified(response);
+
+        //get context details
+        String inContext = getContextInJson(response);
+
+        //send it to view
+        model.addAttribute("inFromWatson", inFromWatson)
+                .addAttribute("inContext", inContext)
+                .addAttribute("dtoTextAndContext", new DtoTextAndContext());
+        return "poi";
+    }
+
+    private String getFromWatsonUnified(MessageResponse response) {
+        ArrayList<String> texts = (ArrayList<String>) response.getOutput().get("text");
+        StringBuilder unifiedText = new StringBuilder();
+        for (String text:texts) {
+            unifiedText.append("\n");
+            unifiedText.append(text);
+        }
+        return unifiedText.toString();
+    }
+
+    private String getContextInJson(MessageResponse response) {
+        Map<String, Object> contextObject = response.getContext();
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = null;
+        try {
+            json = ow.writeValueAsString(contextObject);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
